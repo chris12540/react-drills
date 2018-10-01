0290601cca2c30ac8a730f3c1621b19d045b5566
Christian Ramos <chris12540@gmail.com>
2018-10-01 14:03:12 -0700

    Finish App2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			list: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+		};
+	}
+
+	render() {
+		let message = [];
+		for (const food of this.state.list) {
+			message.push(<h1>{food}</h1>);
+		}
+		return (
+			<div className="App">
+				<ul>{message}</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
