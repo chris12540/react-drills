b720473ee1974554a2bce9c7a30d04037007ce79
Christian Ramos <chris12540@gmail.com>
2018-10-02 16:52:51 -0700

    Finish App-9

---
+++ app-9/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+	return (
+		<nav>
+			<a href="home">Home</a>
+			<a href="signup">Signup</a>
+			<a href="details">Details</a>
+		</nav>
+	);
+};
+
+export default Nav;
