e9846e36313567a3c7c6ed37d43d4dfca807d655
Christian Ramos <chris12540@gmail.com>
2018-10-02 11:51:47 -0700

    Add api for Star Wars

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			people: []
+		};
+	}
+	componentDidMount() {
+		let promise = axios.get("https://swapi.co/api/people");
+		promise.then(res => {
+			this.setState({
+				people: res.data.results
+			});
+		});
+	}
+	render() {
+		let people = this.state.people.map((e, i) => {
+			return <h3 key={i}>{e.name}</h3>;
+		});
+		return (
+			<div className="App">
+				<h1>Star Wars!</h1>
+				{data}
+			</div>
+		);
+	}
 }
 
 export default App;
