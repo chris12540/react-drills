02ced8a1baa9e8428624665bcf9fcea058a7d148
Christian Ramos <chris12540@gmail.com>
2018-10-01 21:27:28 -0700

    Make responsive

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
+import List from "./List";
 import "./App.css";
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			list: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"],
-			search: ""
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      list: List,
+      search: ""
+    };
+  }
 
-	render() {
-		const { list, search } = this.state;
+  render() {
+    const { list, search } = this.state;
 
-		let message = list.filter(item => item.substring(0, search.length) === search).map(item => <h1>{item}</h1>);
+    let message = list
+      .filter(item => item.substring(0, search.length) === search)
+      .map(item => <h2>{item}</h2>);
 
-		return (
-			<div className="App">
-				<input
-					type="text"
-					onChange={e => {
-						this.setState({
-							search: e.target.value
-						});
-					}}
-					placeholder="Type your search"
-				/>
-				{message}
-			</div>
-		);
-	}
+    return (
+      <div className="App">
+        <input
+          type="text"
+          onChange={e => {
+            this.setState({
+              search: e.target.value
+            });
+          }}
+          placeholder="Type your search"
+        />
+        <div id="list">{message}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
